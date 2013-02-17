f7ae14473de4cd2f463ce0c47e68457a8005e517
Edward Powell <powell.518@gmail.com>
2013-02-17 17:58:34 -0500

    Make importer work properly with multi-value rows, add progress indication, error messages, and import summary.

--- search/management/commands/updatedb.py
+++ search/management/commands/updatedb.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 import csv
 import re
+import sys
 
 from search.models import Experiment
 
@@ -30,7 +31,7 @@ class Command(BaseCommand):
                    'control', 'quality']
         if len(args) != 1:
             raise CommandError("Please give one and only one filename.")
-
+        
         #(jfriedly) I considered putting this in a try: except IOError, but I
         # think it's better to just let that bubble up.
         with open(args[0], 'r') as csvfile:
@@ -38,12 +39,26 @@ class Command(BaseCommand):
                                     delimiter='\t')
             # Skip the first row (column names)
             r = reader.next()
-            for line, row in enumerate(reader, start=1):
+            sys.stdout.write(args[0] + ' line:       ')
+            errors = 0
+            adds = 0
+            dupes = 0
+            for line, row in enumerate(reader, start=2):
+                sys.stdout.write('\b'*5 + str(line).rjust(5))
+                sys.stdout.flush()
                 # Sometimes the data has an extra column.  Ignore it.
                 if row.has_key(None):
                     row.pop(None)
-                self.validate_and_add(row, line)
-
+                try:
+                   added, dupe = self.validate_and_add(row, line)
+                   adds += added
+                   dupes += dupe
+                except DBImportError as e:
+                    #TODO make a file of these errors.
+                    errors += 1
+                    sys.stdout.write('\n%s\n%s line:      ' % (e, args[0]))
+            print ('\nAdded %d/%d entries. Ignored %d errors and %d duplicates.'
+                    % (adds, adds+dupes+errors, errors, dupes))
 
     def split_row(self, row, name, depth, line):
         r = DELIMITER.split(row)
@@ -64,7 +79,6 @@ class Command(BaseCommand):
         # The number of delimited values in the row. Supposedly, for each column
         # this will be n where n == 1 or n is the same for all columns in row
         depth = 1
-        row_singles = {}
         row_multis = []
         #NOTE only trans. fac, expt, cell line/organ can have multi-value
 
@@ -73,50 +87,57 @@ class Command(BaseCommand):
                                 % row['gene'])
     
         # Make sure pmid can be made an int, but pass on empty strings.
-        if row['pmid'] != '':
+        if row['pmid'] and row['pmid'].lower() not in ['na', 'n/a']:
             try:
                 int(row['pmid'])
             except ValueError:
-                raise DBImportError("PMIDs must be valid integers. Got '%s'" %
-                                    row['pmid'])
+                raise DBImportError("Error on line %d: PMID must be a valid"
+                                    "integer. Got '%s'" % (line, row['pmid']))
         else:
             row['pmid'] = None
-    
+        
+        if not row['expt_type']:
+            raise DBImportError("Error on line %d: No experiment type." % line)
         # Split up the row, and check that the depth is valid
         expts, depth = self.split_row(row['expt_type'], 'Experiment type', depth, line)
         # If our list of multis is too short, grow it
         while len(row_multis) < depth:
             row_multis.append({})
-        # Then, put each item in the row into the appropriate place
-        for n, expt in enumerate(expts):
-            if len(expts) == 1:
-                row_singles['expt_type'] = expt
-            else:
+        # Then, if there's more than one value, put them in the multi-value list
+        if len(expts) != 1:
+            row.pop('expt_type')
+            for n, expt in enumerate(expts):
                 row_multis[n]['expt_type'] = expt
         
         cell_lines, depth = self.split_row(row['cell_line'], 'Cell line', depth, line)
         while len(row_multis) < depth:
             row_multis.append({})
-        for n, cell_line in enumerate(cell_lines):
-            if len(cell_lines) == 1:
-                row_singles['cell_line'] = cell_line
-            else:
+        if len(cell_lines) == 1:
+            row.pop('cell_line')
+            for n, cell_line in enumerate(cell_lines):
                 row_multis[n]['cell_line'] = cell_line
         
+        if not row['transcription_factor']:
+            raise DBImportError("Error on line %d: No transcription factor." % line)
         transcription_factors, depth = self.split_row(row['transcription_factor'], 'Transcription factor', depth, line)
         while len(row_multis) < depth:
             row_multis.append({})
-        for n, transcription_factor in enumerate(transcription_factors):
-            if len(transcription_factors) == 1:
-                row_singles['transcription_factor'] = transcription_factor
-            else:
+        if len(transcription_factors) != 1:
+            row.pop('transcription_factor')
+            for n, transcription_factor in enumerate(transcription_factors):
                 row_multis[n]['transcription_factor'] = transcription_factor
 
         # Now we can finally add rows
+        added = 0
+        duplicates = 0
         for values in row_multis:
-            values.update(row_singles)
-            e = Experiment(**values)
-            e.save()
+            values.update(row)
+            e, created = Experiment.objects.get_or_create(**values)
+            if not created:
+                duplicates += 1
+            else:
+                added += 1
+        return added, duplicates
 
     def check_experiment_types(self, expt_types):
         """
